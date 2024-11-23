bc4ddc8a0934d295ecde6838321e67316207f8d4
Maicon Takeshi Kamiya <maiconkamiya@hotmail.com>
2024-11-23 11:27:46 -0400

    update

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -28,8 +28,8 @@ app.use(
     origin: process.env.FRONTEND_URL
   })
 );
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.json({limit: '128mb'}));
+app.use(express.urlencoded({limit: '128mb'}));
 app.use(cookieParser());
 app.use(express.json());
 app.use(Sentry.Handlers.requestHandler());
